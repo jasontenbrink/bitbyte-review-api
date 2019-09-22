3606704a9765676ece02909515a6106275972880
jasontenbrink <jason.tenbrink@gmail.com>
2019-09-21 20:03:49 -0500

    adding cors for local development

--- src/server.ts
+++ src/server.ts
@@ -38,6 +38,15 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000') // update to match the domain you will make the request from
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
+
 app.use('/api/registration', registration)
 app.use('/api/login', login)
 app.use('/api/reviews', reviews)
