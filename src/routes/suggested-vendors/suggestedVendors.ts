c918100a53659f6f2ff734305a0bb0d7dc5390fd
jasontenbrink1 <jason@basincommerce.com>
2020-08-16 20:28:26 -0600

    combine suggested vendors with vendors

--- src/routes/suggested-vendors/suggestedVendors.ts
+++ src/routes/suggested-vendors/suggestedVendors.ts
@@ -2,18 +2,35 @@ const express = require('express')
 
 const router = express.Router()
 import { knex } from '../../server'
-import { toDB } from '../../utils'
+import { toDB, fromDB } from '../../utils'
 import { Response } from 'express-serve-static-core'
 const moment = require('moment')
 
-router.post('/', async (req, res: Response) => {
-    try {
-        await knex('suggested_vendors').insert(
-            toDB({ ...req.body, created: moment(), updated: moment() })
-        )
-        res.status(200).send('Suggestion added')
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
+router
+    .post('/', async (req, res: Response) => {
+        try {
+            await knex('vendors').insert(
+                toDB({
+                    ...req.body,
+                    isVerified: false,
+                    created: moment(),
+                    updated: moment(),
+                })
+            )
+            res.status(200).send('Suggestion added')
+        } catch (e) {
+            res.status(500).send(e)
+        }
+    })
+    .get('/', async (req, res) => {
+        try {
+            const suggestedVendors = await knex('vendors').where(
+                'is_verified',
+                false
+            )
+            res.json(fromDB(suggestedVendors))
+        } catch (e) {
+            res.status(500).send(e)
+        }
+    })
 export default router
